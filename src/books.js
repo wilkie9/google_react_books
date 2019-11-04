9af2d30990b434bcb709f6e449f34bbdcf2168c0
wilkie9 <51346820+wilkie9@users.noreply.github.com>
2019-11-04 16:08:17 -0600

    Add files via upload

---
+++ src/books.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'React
+import Book from '.'
+class Books extends Compenent {
+    render()
+{
+    return {
+        <div className='App'>
+        <header/>
+    </div>
+    }
+}}
